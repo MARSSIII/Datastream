1c01a5722d9c25c1bd3ef414061d0f2678d9f997
Alexander Ershov <ershovsanek.centr@yandex.ru>
2025-11-08 23:13:04 +0300

    initial commit

---
+++ src/i18n/i18n.tsx
@@ -0,0 +1,25 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+const resources = {
+  en: {
+    translation: {
+    }
+  },
+  ru: {
+    translation: {
+    }
+  }
+};
+
+i18next
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18next;
