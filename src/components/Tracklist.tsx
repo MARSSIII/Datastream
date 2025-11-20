403e25688d657793d2ade4b9a9652063c3eb778f
Alexander Ershov <ershovsanek.centr@yandex.ru>
2025-11-20 20:42:46 +0300

    add adaptivity for tracklist

--- src/components/Tracklist.tsx
+++ src/components/Tracklist.tsx
@@ -12,7 +12,9 @@ const Tracklist: React.FC<TracklistProps> = ({ tracks, onPlayTrack }) => {
   const { t } = useTranslation();
 
   return (
-    <div className="grid grid-cols-[auto_auto_auto_repeat(7,1fr)] gap-x-6 text-sm">
+    <div className="grid gap-x-6 text-sm
+        grid-cols-[auto_auto_minmax(0,2fr)_minmax(0,1fr)_auto]
+        md:grid-cols-[auto_auto_auto_repeat(7,1fr)]">
 
       <div className="col-span-full grid grid-cols-subgrid gap-x-6 items-center
           text-fg font-bold p-3 border border-fg/10 rounded-t-xl">
@@ -21,15 +23,15 @@ const Tracklist: React.FC<TracklistProps> = ({ tracks, onPlayTrack }) => {
         <p>{t('trackTitle')}</p>
         <p>{t('artist')}</p>
         <p>{t('duration')}</p>
-        <p>{t('played')}</p>
-        <p>{t('quality')}</p>
-        <p>{t('fileSize')}</p>
-        <p>{t('genres')}</p>
-        <p></p>
+        <p className="hidden md:block">{t('played')}</p>
+        <p className="hidden md:block">{t('quality')}</p>
+        <p className="hidden md:block">{t('fileSize')}</p>
+        <p className="hidden md:block">{t('genres')}</p>
+        <p className="hidden md:block"></p>
       </div>
 
       {tracks.map((track, index) => (
-        <div 
+        <div
           key={track.id}
           onClick={() => onPlayTrack(track, tracks)}
           className="col-span-full grid grid-cols-subgrid gap-x-6 items-center px-3
@@ -38,18 +40,18 @@ const Tracklist: React.FC<TracklistProps> = ({ tracks, onPlayTrack }) => {
         >
           <p>0</p>
           <p className="text-center py-3">{index + 1}</p>
-          <p>{track.title}</p>
-          <p>{track.artist}</p>
+          <p className="truncate">{track.title}</p>
+          <p className="truncate">{track.artist}</p>
           <p>{track.duration}</p>
-          <p>{track.plays}</p>
-          <p>{track.quality}</p>
-          <p>{track.size}</p>
-          <p className="truncate">{Array.isArray(track.genres) ? track.genres.join(', ') : ''}</p>
-          <div className="flex justify-center">...</div>
+          <p className="hidden md:block">{track.plays}</p>
+          <p className="hidden md:block">{track.quality}</p>
+          <p className="hidden md:block">{track.size}</p>
+          <p className="hidden md:block truncate">{Array.isArray(track.genres) ? track.genres.join(', ') : ''}</p>
+          <div className="hidden md:flex justify-center">...</div>
         </div>
       ))}
     </div>
   );
-}; 
+};
 
 export default Tracklist;
