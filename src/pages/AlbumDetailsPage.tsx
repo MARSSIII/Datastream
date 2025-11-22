9424fc67a61556c10d88ec47642e33357ec5a3dd
Alexander Ershov <ershovsanek.centr@yandex.ru>
2025-11-22 21:36:06 +0300

    add pointers

--- src/pages/AlbumDetailsPage.tsx
+++ src/pages/AlbumDetailsPage.tsx
@@ -52,20 +52,20 @@ const AlbumDetailsPage: React.FC = () => {
 
       <div className='mt-8 flex gap-4 items-center'>
         <button onClick={() => playTrack(album.tracklist[0], album.tracklist)}
-                className='flex items-center gap-2 px-6 py-2 border border-fg/30 rounded-full hover:scale-105 transition-all'
+                className='flex items-center gap-2 px-6 py-2 border border-fg/30 rounded-full hover:scale-105 transition-all cursor-pointer'
         >
           <PlayIcon className='w-4 h-4 fill-current stroke-current'/>
           <span className='uppercase text-sm'>Play</span>
         </button>
         <button onClick={handleShufflePlay}
-                className='flex items-center gap-2 px-4 py-2 border border-fg/30 rounded-full hover:scale-105 transition-all'
+                className='flex items-center gap-2 px-4 py-2 border border-fg/30 rounded-full hover:scale-105 transition-all cursor-pointer'
         >
           <ShuffleIcon className='w-4 h-4 fill-current stroke-current'/>
           <span className='uppercase text-sm'>Shuffle</span>
         </button>
         <button
                 onClick={() => addTracks(album.tracklist)}
-                className='flex items-center gap-2 px-4 py-2 border border-fg/30 rounded-full hover:scale-105 transition-all'
+                className='flex items-center gap-2 px-4 py-2 border border-fg/30 rounded-full hover:scale-105 transition-all cursor-pointer'
         >
           <PlaylistsIcon className='w-4 h-4 fill-current stroke-current'/>
           <span className='uppercase text-sm'>Add to play queue</span>
